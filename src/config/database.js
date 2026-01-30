5c3709ab21bce4bfdb3dd2ca03670fc30a008a15
julianamilhomens <julianamilhomensdesiqueira@gmail.com>
2026-01-29 23:59:43 -0300

    fix: configure database to use DATABASE_URL in production

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,41 @@
+require('dotenv').config();
+
 module.exports = {
-    dialect: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'postgres',
-    database: 'devburger',
+   
+    ...(process.env.DATABASE_URL
+        ? {
+            url: process.env.DATABASE_URL,
+            dialect: 'postgres',
+        }
+        : {
+            
+            dialect: 'postgres',
+            host: process.env.PG_HOST || 'localhost',
+            port: process.env.PG_PORT || 5432,
+            username: process.env.PG_USERNAME || 'postgres',
+            password: process.env.PG_PASSWORD || 'postgres',
+            database: process.env.PG_DATABASE || 'devburger',
+        }),
+
+   
+    dialectOptions: {
+        ssl: process.env.NODE_ENV === 'production' ? {
+            require: true,
+            rejectUnauthorized: false
+        } : false
+    },
+
+   
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    },
+
+  
+    logging: process.env.NODE_ENV === 'development' ? console.log : false,
+
     define: {
         timestamps: true,
         underscored: true,
