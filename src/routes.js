f808b6147a631046ef49fb3011229b07dec312bf
julianamilhomens <julianamilhomensdesiqueira@gmail.com>
2026-02-04 00:59:27 -0300

    fix: move auth middleware to protected routes only

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-import { Router } from 'express'
+import { Router } from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
 import authMiddleware from './app/middlewares/auth';
@@ -10,27 +10,28 @@ import CategoryController from './app/controllers/CategoryController';
 import OrderController from './app/controllers/OrderController';
 import CreatePaymentIntentController from './app/controllers/stripe/CreatePaymentIntentController';
 
-const routes = new Router()
-
+const routes = new Router();
 const upload = multer(multerConfig);
+
 
 routes.post('/users', UserController.store);
 routes.post('/session', SessionController.store);
 
-routes.use(authMiddleware);
-routes.post('/products', upload.single('file'), ProductController.store);
 routes.get('/products', ProductController.index);
+routes.get('/categories', CategoryController.index);
+
+routes.use(authMiddleware);
+
+routes.post('/products', upload.single('file'), ProductController.store);
 routes.put('/products/:id', upload.single('file'), ProductController.update);
 
 routes.post('/categories', upload.single('file'), CategoryController.store);
-routes.get('/categories', CategoryController.index);
 routes.put('/categories/:id', upload.single('file'), CategoryController.update);
 
 routes.post('/orders', OrderController.store);
-routes.put('/orders/:id', OrderController.update);
 routes.get('/orders', OrderController.index);
+routes.put('/orders/:id', OrderController.update);
 routes.delete('/orders/:id', OrderController.delete);
-
 
 routes.post('/create-payment-intent', CreatePaymentIntentController.store);
 
